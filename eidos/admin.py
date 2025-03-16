e3b92c0d6ea6acb29257f9c93ff48f0c07cdf6a4
Bosnape <pcabrejosm@gmail.com>
2025-03-16 11:05:42 -0500

    Added admin.site.register for the model Account

--- eidos/admin.py
+++ eidos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Account
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'is_staff', 'date_joined')
+    search_fields = ('email',)
+    readonly_fields = ('date_joined',)
+    ordering = ('email',)
+    
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+admin.site.register(Account, CustomUserAdmin)
