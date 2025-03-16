9758fe3ea30c29658f0cb2ba8b1490d936ea88aa
Bosnape <pcabrejosm@gmail.com>
2025-03-16 11:17:11 -0500

    Modified the urlpatterns to include the urls of each app

--- eidosapp/urls.py
+++ eidosapp/urls.py
@@ -15,24 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf.urls.static import static
 from django.conf import settings
 
-from eidos import views as eidos
-from business import views as business
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', eidos.home, name="home"),
-    path("contact/", eidos.contact, name="contact"), 
-    path('business/', eidos.business, name="business"),
-    path('get_started/', eidos.business_form, name="get_started"),
-    path('get_started/welcome/', eidos.welcome, name="welcome"),
-    path('dashboard/<str:name>/', business.dashboard, name="dashboard"),
-    path('dashboard/<str:name>/services', business.services, name="services"),
-    path('<str:name>/', business.profile, name="business_profile"),
+    path('', include('eidos.urls')),
+    path('business/', include('business.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
