8b49457c452410ff6802f303e9d12e477c7a67c1
Bosnape <pcabrejosm@gmail.com>
2025-03-16 11:23:04 -0500

    Deleted files and modified migrations

--- eidos/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from business.models import Business
-
-class BusinessForm(forms.ModelForm):
-
-    class Meta:
-        model = Business
-        fields = ['name', 'logo', 'address', 'email', 'phone']
-        
