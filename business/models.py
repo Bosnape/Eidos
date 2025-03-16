75f244dc99b8867f21fd4418db3207285972fef8
Bosnape <pcabrejosm@gmail.com>
2025-03-16 17:02:33 -0500

    Added Service model and added new attributes for the social media in BusinessAccount model

--- business/models.py
+++ business/models.py
@@ -10,6 +10,11 @@ class BusinessAccount(models.Model):
     logo = models.ImageField(upload_to='business/images/')
     user = models.OneToOneField(Account, on_delete=models.CASCADE, null=True, blank=True)
     phone = models.CharField(max_length=20)
+    
+    instagram = models.URLField(blank=True, null=True)
+    facebook = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+    tiktok = models.URLField(blank=True, null=True) 
    
     def __str__(self):
         return self.name
@@ -26,3 +31,18 @@ class RegistrationSession(models.Model):
     
     def __str__(self):
         return f"Session: {self.session_key}"
+    
+def get_default_business():
+    return BusinessAccount.objects.first().id 
+
+class Service(models.Model):
+    business = models.ForeignKey(BusinessAccount, on_delete=models.CASCADE, default=get_default_business)  
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='services/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
