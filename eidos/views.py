208852a474f42520c76c8b3ac1b0501f9b448bde
Bosnape <pcabrejosm@gmail.com>
2025-03-16 11:01:10 -0500

    Adapted the searchBusiness function to the login aspect and deleted the showRegistrationConfirmation function

--- eidos/views.py
+++ eidos/views.py
@@ -1,29 +1,16 @@
-from django.shortcuts import render, redirect
-from .forms import BusinessForm
-from business.models import Business
+from django.shortcuts import render
+from business.models import BusinessAccount
 
-def home(request): 
+def showEidos(request): 
     return render(request, "home.html") 
-  
-def contact(request): 
+
+def showContactInfo(request): 
     return render(request, "contact.html")
 
-def business(request):
+def searchBusiness(request):
     searchTerm = request.GET.get('searchBusiness')
     if searchTerm:
-        businesses = Business.objects.filter(name__icontains=searchTerm)
+        businesses = BusinessAccount.objects.filter(name__icontains=searchTerm)
     else:
-        businesses = Business.objects.all()
-    return render(request, 'business.html', {'searchTerm': searchTerm, 'businesses': businesses})
-
-def business_form(request):
-    if request.POST:
-        form = BusinessForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-        return redirect('welcome')
-        
-    return render(request, "form.html", {'form': BusinessForm})
-
-def welcome(request):
-    return render(request, "welcome.html", {'business': Business.objects.last()})
+        businesses = BusinessAccount.objects.all()
+    return render(request, 'search_business.html', {'searchTerm': searchTerm, 'businesses': businesses})
