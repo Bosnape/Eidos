3804d5d4a0814e1ee843db26b99612ef3deb943a
Bosnape <pcabrejosm@gmail.com>
2025-03-18 20:05:27 -0500

    Added admin.site.register for Employee, Tag, and Portfolio (with their respective admin class)

--- business/admin.py
+++ business/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BusinessAccount, RegistrationSession, Service
+from .models import BusinessAccount, RegistrationSession, Service, Employee, Tag, PortfolioItem
 
 class BusinessAccountAdmin(admin.ModelAdmin):
     list_display = ('name', 'user', 'phone')
@@ -12,7 +12,22 @@ class RegistrationSessionAdmin(admin.ModelAdmin):
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'business', 'price', 'available')
     search_fields = ('name', 'business__name')
+    
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'business', 'phone')
+    search_fields = ('name', 'business__name')
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class PortfolioItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'business', 'created_at')
+    search_fields = ('title', 'business__name')
 
 admin.site.register(BusinessAccount, BusinessAccountAdmin)
 admin.site.register(RegistrationSession, RegistrationSessionAdmin)
 admin.site.register(Service, ServiceAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(PortfolioItem, PortfolioItemAdmin)
