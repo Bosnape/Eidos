4a46448fcdbbc93d2e050dff4fba6102bee63905
Bosnape <pcabrejosm@gmail.com>
2025-03-18 20:16:10 -0500

    Changed the function's name to show the home page

--- eidos/urls.py
+++ eidos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.showEidos, name="home"),
+    path('', views.showEidosHome, name="home"),
     path('contact/', views.showContactInfo, name="contact"), 
     path('business/', views.searchBusiness, name="search_business"),
 ]
