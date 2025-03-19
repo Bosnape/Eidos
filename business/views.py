dec9b2f82490ddb599be43deed040ec150cf64c0
Bosnape <pcabrejosm@gmail.com>
2025-03-18 21:14:30 -0500

    Modified migrations and database

--- business/views.py
+++ business/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.urls import reverse
 import os
 from .models import BusinessAccount, RegistrationSession, Account, Service, Employee, PortfolioItem
 from .forms import BusinessInfoForm, BusinessUserForm, LoginForm, BusinessProfileForm, ServiceForm, SocialMediaForm, EmployeeForm, PortfolioItemForm
