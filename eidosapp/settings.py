a2ddc0eed2b87feeeecd817b933eaf3de5423f26
Bosnape <pcabrejosm@gmail.com>
2025-04-17 02:18:04 -0500

    Added STATICFILES_DIRS

--- eidosapp/settings.py
+++ eidosapp/settings.py
@@ -97,6 +97,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'eidosapp/static/'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
