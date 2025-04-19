eeb784da7a2b0e9a307ecca1dedf5f7d8c619d19
JuanJose <jjrodrigur@eafit.edu.co>
2025-04-19 18:13:09 -0500

    added customer admin

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from customer.models import Customer
 
-# Register your models here.
+User = get_user_model()
+
+# Crear una clase personalizada para administrar el modelo Customer
+class CustomerAdmin(admin.ModelAdmin):
+    
+    def delete_model(self, request, obj):
+        obj.user.delete()  
+        super().delete_model(request, obj)  
+
+    list_display = ('user', 'first_name', 'last_name', 'identification')  
+
+    search_fields = ('first_name', 'last_name', 'identification')
+
+admin.site.register(Customer, CustomerAdmin)
