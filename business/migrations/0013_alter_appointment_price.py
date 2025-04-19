21ce7d62feb5b6a84d2d0caac3527659e1328fb4
JuanJose <jjrodrigur@eafit.edu.co>
2025-04-19 15:48:59 -0500

    Migration update for compatibility with the customer app

--- business/migrations/0013_alter_appointment_price.py
+++ business/migrations/0013_alter_appointment_price.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('business', '0012_customer'),
+        ('business', '0011_appointment'),
     ]
 
     operations = [
