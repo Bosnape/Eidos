3aeb892be23b81be92b6504b8bd729b826974048
JuanJose <jjrodrigur@eafit.edu.co>
2025-04-19 15:42:38 -0500

    Customer model Removed

--- business/models.py
+++ business/models.py
@@ -90,12 +90,3 @@ class Appointment(models.Model):
 
     def __str__(self):
         return f"Appointment for {self.customer_name} at {self.date} - {self.time}"
-
-class Customer(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    identification = models.CharField(max_length=20, unique=True)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
