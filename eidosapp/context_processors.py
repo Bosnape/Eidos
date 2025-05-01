89adc0fbc20d7582349392fa6138580fda95df02
Bosnape <pcabrejosm@gmail.com>
2025-05-01 18:06:35 -0500

    Added a new function to retrieve the businessAccount of the current authenticated user

--- eidosapp/context_processors.py
+++ eidosapp/context_processors.py
@@ -1,3 +1,5 @@
+from business.models import BusinessAccount
+
 def user_type_context(request):
     user_type = None
     if request.user.is_authenticated:
@@ -6,3 +8,9 @@ def user_type_context(request):
         elif hasattr(request.user, 'is_customer') and request.user.is_customer:
             user_type = 'customer'
     return {'user_type': user_type}
+
+def business_user_context(request):
+    business_user = None
+    if request.user.is_authenticated and hasattr(request.user, 'is_business') and request.user.is_business:
+        business_user = BusinessAccount.objects.get(user=request.user)
+    return {'business_user': business_user}
